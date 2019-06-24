60bd9a324755418edd7316cb68f5c8ff57d9ca8d
chanduse <48502518+chanduse@users.noreply.github.com>
2019-06-24 18:50:06 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.alt=restaurant.name;
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.src = "./"+DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h1');
